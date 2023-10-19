afac939bd77111e7d9aad285ac199a9948459725
Britney Abner <brodytabner@protonmail.com>
2023-10-19 18:51:31 -0500

    implemented BlockingFifoQueue using bouded buffer design, implemented new solution for the race condition

--- src/edu/utdallas/taskExecutorImpl/BlockingFifoQueueImpl.java
+++ src/edu/utdallas/taskExecutorImpl/BlockingFifoQueueImpl.java
@@ -10,60 +10,51 @@ public class BlockingFifoQueueImpl implements BlockingFifoQueue {
   private int nextIn, nextOut;
   private int count;
   private Object notFull, notEmpty; // locks used for synchronization
-  private boolean notifiedHasExecuted = 1; // if true, thread waiting to put
-                                          // has been notified, has executed
-                                          // and has incrmented count.
-                                          // solves race condition
 
 	public BlockingFifoQueueImpl(int bufferSize) {
 		BUFFER_SIZE = bufferSize;
 		buffer = new Task[BUFFER_SIZE];
 	}
 	
-  // places task at end of FIFO
+  // places task at back of FIFO
 	@Override
 	public void put(Task newTask) {
-		// if waiting thread that has been notified has not executed, cant put
-    if(notifiedHasExecuted = 0)
-    {
-      notFull.wait();
-    }
-    :
     // if buffer is full, wait for take
-    if(count == BUFFER_SIZE)
+    // for loop prevents a race condition as waiting thread must recheck
+    // BUFFER_SIZE after notified by take()
+    for(count == BUFFER_SIZE)
     {
       notFull.wait();
-      notifiedHasExecuted = 0; // signals thread notified still needs to execute
-    }
-
-      // puts task at end of FIFO, increments nextIn and count
-      buffer[nextIn] = task; 
-      nextIn = (nextIn + 1) % BUFFER_SIZE;
-      count++;
-
-      notifiedHasExecuted = 1; // signals that notified thread has executed
-      notEmpty.notify(); // signal waiting take threads
-    }
-	}
-
-  // takes task from front of FIFO, returns the taskExecutor
-  // increments nextOut and count
-	@Override
-	public Task take() {
-    // if buffer is empty, wait for put
-    if(count == 0 )
-    {
-      notEmpty.wait();
     }
     
-    // sets return value for task, incremtns nextOut, decrements count 
-    Task result = buffer[nextOut];
-    nextOut = (nextOunt + 1) % BUFFER_SIZE;
-    count --;
+    // puts task at end of FIFO, increments nextIn and count
+    buffer[nextIn] = task; 
+    nextIn = (nextIn + 1) % BUFFER_SIZE;
+    count++;
 
-    notFull.nofity(); // signal waiting put threads
-    return result;
+    notEmpty.notify(); // signal waiting take threads
+    }
 	}
-	
 
+  // takes task from front of FIFO, returns the task
+	@Override
+	public Task take() {
+    synchronized(this) { // only one thread can take at a time,
+                         // prevents race condition
+
+      // if buffer is empty, wait for put
+      if(count == 0)
+      {
+        notEmpty.wait();
+      }
+    
+      // sets return value for task, incremtns nextOut, decrements count 
+      Task result = buffer[nextOut];
+      nextOut = (nextOut + 1) % BUFFER_SIZE;
+      count --;
+
+      notFull.nofity(); // signal waiting put threads
+      return result;
+    }
+  }
 }
